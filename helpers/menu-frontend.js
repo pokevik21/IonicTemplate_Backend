13cd1af3332038c3e8f94ffd269620926130b7ab
pokevik21 <pokevik21@gmail.com>
2020-10-09 16:12:29 +0200

    IonicTemplate ajustes

--- helpers/menu-frontend.js
+++ helpers/menu-frontend.js
@@ -3,29 +3,27 @@ const getMenuFrontEnd = ( role ) => {
     const menu = 
     [
         {
-          titulo: 'Principal',
-          icon: 'mdi mdi-gauge',
-          submenus: [
-            { titulo: 'Dashboard', url: '/dashboard' },
-            { titulo: 'ProgressBar', url: '/dashboard/progress' },
-            { titulo: 'Gráficas', url: '/dashboard/grafica1' },
-            { titulo: 'Promesas', url: '/dashboard/promesas' },
-            { titulo: 'Rxjs', url: '/dashboard/rxjs' }
+          title: 'Principal',
+          submenu: [
+            { title: 'Dashboard', url: '/app', icon: 'speedometer-outline' },
+            { title: 'ProgressBar', url: '/app/progress', icon: 'battery-half-outline'},
+            { title: 'Gráficos', url: '/app/graficos', icon: 'stats-chart-outline' },
+            { title: 'Promesas', url: '/app/promesas', icon: 'pulse-outline'},
+            { title: 'Rxjs', url: '/app/rxjs', icon: 'shapes-outline' }
           ]
         },
         {
-          titulo: 'Mantenimiento',
-          icon: 'mdi mdi-folder-lock-open',
-          submenus: [
-            // { titulo: 'Usuarios', url: '/dashboard/usuarios' },
-            { titulo: 'Hospitales', url: '/dashboard/hospitales' },
-            { titulo: 'Médicos', url: '/dashboard/medicos' }
+          title: 'Mantenimiento',
+          submenu: [
+            // { title: 'Usuarios', url: '/app/usuarios' },
+            { title: 'Hospitales', url: '/app/hospitales', icon: 'business-outline' },
+            { title: 'Médicos', url: '/app/medicos', icon: 'thermometer-outline' }
           ]
         }
       ];
 
     if (role === 'ADMIN_ROLE') {
-        menu[1].submenus.unshift({ titulo: 'Usuarios', url: '/dashboard/usuarios' });
+        menu[1].submenu.unshift({ title: 'Usuarios', url: '/app/usuarios', icon: 'people-circle-outline' });
     }
     
     return menu;
